7825a6f8caebd30637f36a53791696cdfbaea4be
Mattia <zingaretti.mattia@gmail.com>
2021-03-29 21:35:36 +0200

    started FE bio.js

--- app/static/js/bio.js
+++ app/static/js/bio.js
@@ -1,19 +1,23 @@
 
 // Make an instance of two and place it on the page.
 var d = document.getElementById('d-start');
-console.log(d);
+
 var params = { width: 285, height: 200 };
 var two = new Two(params).appendTo(d);
 
 
 // two has convenience methods to create shapes.
-var circle = two.makeCircle(72, 100, 50);
+var circle = two.makeCircle(100, 100, 10);
 var rect = two.makeRectangle(213, 100, 100, 100);
+var path = two.makePath(100, 100, 100 , 150);
+var text = new  Two.Text("Test", 0 , 0  );
+
+text.visible = true;
 
 // The object returned has many stylable properties:
 circle.fill = '#FF8000';
 circle.stroke = 'orangered'; // Accepts all valid css color
-circle.linewidth = 5;
+circle.linewidth = 3;
 
 rect.fill = 'rgb(0, 200, 255)';
 rect.opacity = 0.75;
