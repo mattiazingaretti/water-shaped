bcfc394b2398efa3d3e70595a94125fe99b6fca5
Mattia <zingaretti.mattia@gmail.com>
2021-08-01 17:30:41 +0200

    Recovering contact page functionality

--- app/contact/utils.py
+++ app/contact/utils.py
@@ -4,7 +4,6 @@ from app.config import SSL_PORT,SMTP_SERVER,SENDER_EMAIL,RECEIVER_EMAIL,SENDER_E
 
 def setMessage(sender, subject, message):
     message ="""Subject: [WS] Someone wants to talk to you.
-    
     Who ? : {0} \n  What he\she wants ? : {1} \n
     \n
     \n
@@ -14,12 +13,11 @@ def setMessage(sender, subject, message):
     return message
 
 def sendEmail(message):
-    #TODO add better error handling
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(SMTP_SERVER, SSL_PORT, context=context) as server:
         server.login(SENDER_EMAIL, SENDER_EMAIL_PSW)
         server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message)
-
+    
 
 
 
